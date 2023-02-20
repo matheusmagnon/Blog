955e69015cf9e0ca84127d6ea709af7881ffe6eb
matheusmagnon@gmail.com <matheusmagnon@gmail.com>
2023-02-20 09:52:33 -0300

    ii

--- src/components/CardRepository/index.tsx
+++ src/components/CardRepository/index.tsx
@@ -3,40 +3,35 @@ import { api } from "../../lib/axios";
 import { CardContainer, CardContente, CardHeader, CardText } from "./styles";
 
 type Fetch = {
+  id: number;
   title: string;
   created_at: Date;
   body: string;
 };
 
 export function CardRepository() {
-  const [fetch, setFetch] = useState<Fetch>();
+  const [fetchRepositorys, setFetchRepositorys] = useState<Fetch[]>();
 
   const fetchProfile = async () => {
+    const idRepository = 1;
     const response = await api.get(
-      "/repos/matheusmagnon/projetos-portifolio/issues/1 "
+      `/repos/matheusmagnon/projetos-portifolio/issues/${idRepository}`
     );
-    // setProfile(() => {
 
-    // });
-
-    setFetch(response.data);
+    setFetchRepositorys(response.data);
   };
 
   useEffect(() => {
     fetchProfile();
   }, []);
 
-  console.log(fetch);
+  console.log(fetchRepositorys);
 
   return (
     <CardContainer>
       <CardContente>
-        <CardHeader>
-          <h3>{fetch?.title}</h3>
-          <span>{fetch?.created_at}</span>
-        </CardHeader>
         <CardText>
-          {fetch?.body}
+          {/* {fetch?.body} */}
           Programming languages all have built-in data structures, but these
           often differ from one language to another. This article attempts to
           list the built-in data structures available in...
